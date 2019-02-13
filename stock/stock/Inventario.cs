d68e8142ac8b38586f1d53b3976d71aaed66051a
GusZeta <guszeta1918@gmail.com>
2019-02-13 08:08:03 -0400

    stock

---
+++ stock/stock/Inventario.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stock
+{
+    class Inventario
+    {
+        public delegate void delegado (int n);
+        public event delegado stockminimo;
+        private int stock;
+        public Inventario()
+        {
+            this.stock = 0;
+        }
+        public void incrementarstock(int n)
+        {
+            this.stock += n;
+            if (this.stock < 5 && stockminimo != null)
+            {
+                stockminimo(this.stock);
+            }
+        }
+        public void disminuirstock(int n)
+        {
+            if (this.stock < n)
+            {
+                Console.WriteLine("No se dispone de esa cantidad");
+            }
+            else
+            {
+                this.stock -= n;
+            }
+            if (this.stock < 5 && stockminimo != null)
+            {
+                stockminimo(this.stock);
+            }
+        }
+        public void mostrarstock()
+        {
+            Console.WriteLine("el stock actual es: {0}", this.stock);
+        }
+        
+
+    }
+}
---
+++ stock/stock/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace stock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inventario inv = new Inventario();
+            inv.stockminimo += jeje;
+            int n;
+            Boolean salir = false;
+            while (salir == false)
+            {
+
+                Console.Clear();
+                Console.WriteLine("1. Incrementar");
+                Console.WriteLine("2. Disminuir");
+                Console.WriteLine("3. Mostrar");
+                Console.WriteLine("4. Salir");
+                Console.WriteLine("Elegir una opcion");
+                int ope = int.Parse(Console.ReadLine());
+                switch (ope)
+                {
+                    case 1:
+                        Console.WriteLine("Introduzca n");
+                        n = int.Parse(Console.ReadLine());
+                        inv.incrementarstock(n);
+                        break;
+                    case 2:
+                        Console.WriteLine("Introduzca n");
+                        n = int.Parse(Console.ReadLine());
+                        inv.disminuirstock(n);
+                        break;
+                    case 3:
+                        inv.mostrarstock();
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        salir = true;
+                        break;
+                    default:
+                        Console.WriteLine("Accion no valida");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+        static void jeje(int n)
+        {
+            Console.WriteLine("El stock esta en su minimo {0}", n);
+            Console.ReadKey();
+        }
+
+    }
+    
+}
