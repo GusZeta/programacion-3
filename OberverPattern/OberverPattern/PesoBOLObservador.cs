b6522ee2c341d4740c2b7d7bbcb154030a4f7e7e
GusZeta <guszeta1918@gmail.com>
2019-04-02 08:31:40 -0400

    Cambio

---
+++ OberverPattern/OberverPattern/PesoBOLObservador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OberverPattern
+{
+    class PesoBOLObservador:Observador
+    {
+        private double valorCambio = 6.96;
+
+        public PesoBOLObservador(Subject sujeto)
+        {
+            this.sujeto = sujeto;
+            this.sujeto.agregar(this); 
+        }
+
+        public override void actualizar()
+        {
+            Console.WriteLine("BOL: " + (sujeto.getEstado() * valorCambio));
+        }
+
+    }
+}
---
+++ OberverPattern/OberverPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OberverPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject subject = new Subject();
+            new SolObservador(subject);
+            new PesoARGObservador(subject);
+            new PesoMXObservador(subject);
+            new PesoBOLObservador(subject);
+            Console.WriteLine("Si desea cambiar 10 dólares obtendrá : ");
+            subject.setEstado(10);
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Si desea cambiar 100 dólares obtendrá : ");
+            subject.setEstado(100);
+            Console.ReadLine();
+        }
+    }
+}
