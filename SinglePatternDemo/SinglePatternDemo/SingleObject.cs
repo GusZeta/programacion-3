4e1e3c12ca2b53c866bc95ea10354107cdf79530
GusZeta <guszeta1918@gmail.com>
2019-04-02 07:09:46 -0400

    Figura

---
+++ SinglePatternDemo/SinglePatternDemo/SingleObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglePatternDemo
+{
+    class SingleObject
+    {
+        private static SingleObject instace = null;
+
+        private SingleObject()
+        {
+
+        }
+        public static SingleObject getInstance()
+        {
+            if (instace == null)
+                return new SingleObject();
+            else
+                return instace;
+        }
+        public void ShowMethod()
+        {
+            Console.WriteLine("soy singleton");
+        }
+    }
+}
