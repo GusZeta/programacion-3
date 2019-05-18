575999aed98cf92ecb00ffa5ee76a613b27db38c
GusZeta <guszeta1918@gmail.com>
2019-05-18 09:45:08 -0400

    Parcial 2

--- 2do parcial/2do parcial p3 cliente/2do parcial p3 cliente/Form1.cs
+++ 2do parcial/2do parcial p3 cliente/2do parcial p3 cliente/Form1.cs
@@ -12,6 +12,14 @@ namespace _2do_parcial_p3_cliente
 {
     public partial class Form1 : Form
     {
+        const String ruta1 = "C:\\Users\\Estudiante\\programacion-3\\2do parcial\\2do parcial p3 cliente\\1.png";
+        const String ruta2 = "C:\\Users\\Estudiante\\programacion-3\\2do parcial\\2do parcial p3 cliente\\2.png";
+        const String ruta3 = "C:\\Users\\Estudiante\\programacion-3\\2do parcial\\2do parcial p3 cliente\\3.png";
+        const String ruta4 = "C:\\Users\\Estudiante\\programacion-3\\2do parcial\\2do parcial p3 cliente\\4.png";
+        const String ruta5 = "C:\\Users\\Estudiante\\programacion-3\\2do parcial\\2do parcial p3 cliente\\5.png";
+        const String ruta6 = "C:\\Users\\Estudiante\\programacion-3\\2do parcial\\2do parcial p3 cliente\\6.png";
+        const String ruta7 = "C:\\Users\\Estudiante\\programacion-3\\2do parcial\\2do parcial p3 cliente\\7.png";
+        const String ruta8 = "C:\\Users\\Estudiante\\programacion-3\\2do parcial\\2do parcial p3 cliente\\8.png";
         public Form1()
         {
             InitializeComponent();
