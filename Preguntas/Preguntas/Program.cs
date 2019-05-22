191e7d8a8d293340f66c69470e67e853065c780e
GusZeta <guszeta1918@gmail.com>
2019-05-22 08:23:55 -0400

    Practica 1

---
+++ Preguntas/Preguntas/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Preguntas
+{
+    class Program
+    {
+        public static void Tiempo()
+        {
+            
+            Thread.Sleep(6000);
+        }
+        public static void Preguntas()
+        {
+            string y;
+            int x = 0;
+            Console.WriteLine("¿Cual es la capital de Bolivia?");
+            y = Console.ReadLine();
+            if (y == "Sucre")
+            {
+                x++;
+            }
+            Console.WriteLine("¿Cuantos departamentos tiene Bolivia?");
+            y = Console.ReadLine();
+            if (y == "7" || y == "siete")
+            {
+                x++;
+            }
+            Console.WriteLine("¿cuanto es (((2+3)*4)/10)*9?");
+            y = Console.ReadLine();
+            if (y == "18")
+            {
+                x++;
+            }
+            Console.WriteLine("¿Cual es la recompensa actual de Luffy?");
+            y = Console.ReadLine();
+            if (y == "1500000000")
+            {
+                x++;
+            }
+            Console.WriteLine("¿Cuantas estrellas tiene Estados Unidos?");
+            y = Console.ReadLine();
+            if (y == "50" || y == "cincuenta")
+            {
+                x++;
+            }
+            Console.WriteLine("(x+4)+(-5x-6)=-30 ¿x?");
+            y = Console.ReadLine();
+            if (y == "7")
+            {
+                x++;
+            }
+            Console.WriteLine("¿Cuantos años tiene FOX?");
+            y = Console.ReadLine();
+            if (y == "21")
+            {
+                x++;
+            }
+            Console.WriteLine("¿Año del primer grito libertario?");
+            y = Console.ReadLine();
+            if (y == "1809")
+            {
+                x++;
+            }
+            Console.WriteLine("¿Velocidad de un cuerpo que rrecorrio 5 Km en 5 minutos?");
+            y = Console.ReadLine();
+            if (y == "16,67")
+            {
+                x++;
+            }
+            Console.WriteLine("¿Cuanto es 2+2?");
+            y = Console.ReadLine();
+            if (y == "4")
+            {
+                x++;
+            }
+            Console.WriteLine("el puntaje es: " + x);
+        }
+        static void Main(string[] args)
+        {
+            Thread p = new Thread(new ThreadStart(Preguntas));
+            Thread t = new Thread(new ThreadStart(Tiempo));
+            p.Start();
+            t.Start();
+            t.Join();
+            p.Abort();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ operaciones de un vector/operaciones de un vector/Vector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operaciones_de_un_vector
+{
+    class Vector
+    {
+        int[] x;
+        int[] y;
+        int n=int.Parse(Console.ReadLine());
+        public static Vector Instancia;
+        public constructor(int[] a, int[]b)
+        {
+            int n;
+            n =int.Parse(Console.ReadLine());
+            a =new int [n];
+            b = new int[n];
+            for (int i = 0; i < a.Length; i++)
+            {
+                n = int.Parse(Console.ReadLine());
+                a[i] = n;
+            }
+            for (int i = 0; i < b.Length; i++)
+            {
+                n = int.Parse(Console.ReadLine());
+                b[i] = n;
+            }
+            x = a;
+            y = b;
+        }
+        public int Sumar()
+        {
+           return x[n] + y[n];
+        }
+        public int Resta()
+        {
+            return x[n] - y[n];
+        }
+        public int Multiplicar()
+        {
+            return x[n] * y[n];
+        }
+    }
+}
