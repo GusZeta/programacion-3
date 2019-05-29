deac577dfd72bfaa56dd6d0e4c03d1e4214370b6
GusZeta <guszeta1918@gmail.com>
2019-05-29 08:47:21 -0400

    android juego

---
+++ juego/juego/MainActivity.cs
@@ -0,0 +1,71 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace juego
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            EditText letra = FindViewById<EditText>(Resource.Id.txtLetra);
+            Button enviar = FindViewById<Button>(Resource.Id.btnEnviar);
+            TextView resultado = FindViewById<TextView>(Resource.Id.lblLetra);
+            EditText palabra = FindViewById<EditText>(Resource.Id.txtPalabra);
+            Button enviarPalabra = FindViewById<Button>(Resource.Id.btnpalabra);
+            ImageView imagen = FindViewById<ImageView>(Resource.Id.imag);
+            void actualizar()
+            {
+                switch (juego.ahorcado.getInstancia().Estado)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        imagen.SetImageResource(Resource.Drawable.uno);
+                        break;
+                    case 2:
+                        imagen.SetImageResource(Resource.Drawable.dos);
+                        break;
+                    case 3:
+                        imagen.SetImageResource(Resource.Drawable.tres);
+                        break;
+                    case 4:
+                        imagen.SetImageResource(Resource.Drawable.cuatro);
+                        break;
+                    case 5:
+                        imagen.SetImageResource(Resource.Drawable.cinco);
+                        break;
+                    case 6:
+                        imagen.SetImageResource(Resource.Drawable.seis);
+                        break;
+                    case 7:
+                        imagen.SetImageResource(Resource.Drawable.siete);
+                        break;
+                    case 8:
+                        imagen.SetImageResource(Resource.Drawable.ocho);
+                        break;
+                }
+            }
+            enviar.Click += (sender, e) =>
+              {
+                  char l = letra.Text[0];
+                  string res = juego.ahorcado.getInstancia().Buscar(l);
+                  resultado.Text = res;
+                  actualizar();
+              };
+            enviarPalabra.Click += (sender, e) =>
+            {
+                string p = palabra.Text;
+                string resu = juego.ahorcado.getInstancia().BuscarPalabra(p);
+                resultado.Text = resu;
+                actualizar();
+            };
+        }
+    }
+}
---
+++ juego/juego/ahorcado.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace juego
+{
+    class ahorcado
+    {
+        private static readonly string[] Palabras = { "computacion", "español", "eucalipto", "orquidea" };
+        private String PalabraAdivinar;
+        private List<char> Letras;
+        public int Estado { get; private set; }
+        private String Guiones;
+        private static ahorcado Instancia;
+        private ahorcado()
+        { //singleton
+            Iniciar();
+        }
+        public void Iniciar()
+        {
+            Estado = 0;
+            Letras = new List<char>();
+            Random rnd = new Random();
+            int indice = rnd.Next(3);
+            PalabraAdivinar = Palabras[indice];
+            Guiones = "";
+            Guiones = Guiones.PadLeft(PalabraAdivinar.Count(), '-');
+        }
+        public String Buscar(char Letra)
+        {
+            Letras.Add(Letra);
+            String aux = "";
+            if (PalabraAdivinar.Contains(Letra.ToString()))
+            {
+                for (int i = 0; i < PalabraAdivinar.Count(); i++)
+                {
+
+                    if (PalabraAdivinar[i] == Letra)
+                    {
+                        aux += Letra;
+                    }
+                    else
+                    {
+                        aux += Guiones[i];
+                    }
+                }
+                Guiones = aux;
+                return aux;
+
+            }
+            else
+            {
+                Estado++;
+                return Guiones;
+            }
+        }
+        public String BuscarPalabra(String Palabra)
+        {
+            if (Palabra == PalabraAdivinar)
+            {
+                Guiones = PalabraAdivinar;
+            }
+            else
+            {
+                Estado++;
+            }
+            return Guiones;
+
+        }
+
+        public static ahorcado getInstancia()
+        {
+            if (Instancia == null)
+            {
+                Instancia = new ahorcado();
+            }
+            return Instancia;
+        }
+
+    }
+}
